839838bb2dea25afe782e98d25c7f9d2d180face
ani.albano <62104966+ani-albano@users.noreply.github.com>
2020-10-18 16:03:34 -0300

    Add files via upload

---
+++ Anagram.js
@@ -0,0 +1,27 @@
+// Funcion para determinar si dos palabras son anagramas
+function isAnagram(word1, word2) {
+    if(typeof word1 !== "string" || typeof word2 !== "string") {
+        return false;
+    }
+
+    const lookup = {};
+
+    /* Loop on the first string */
+    for(let i = 0; i < word1.length; i++) {
+        let letter = word1[i]; //if letter exist increment, otherwise, 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    /* Loop on the second */
+    for(let i = 0; i < word2.length; i++) {
+        let letter = word2[i];
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true;
+}
+
+isAnagram("patata", "tatapaz")
